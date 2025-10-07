258bb94a568218a51b12015047f7a4cff1f315b2
victor <vhernandezt24@fpcoruna.afundacion.org>
2025-10-07 21:32:09 +0200

    ejercicio 4 carrito.php

---
+++ sprint1apache/www/site3/carrito.php
@@ -0,0 +1,18 @@
+<?php
+$carrito = [
+    "Manzana" => 0.5,
+    "Pan" => 1.2,
+    "Leche" => 0.9
+];
+
+$total = 0;
+echo "<table border='1'><tr><th>Producto</th><th>Precio (€)</th></tr>";
+
+foreach ($carrito as $producto => $precio) {
+    echo "<tr><td>$producto</td><td>" . number_format($precio, 2) . "€</td></tr>";
+    $total += $precio;
+}
+
+echo "<tr><td><b>TOTAL</b></td><td><b>" . number_format($total, 2) . "€</b></td></tr>";
+echo "</table>";
+?>
