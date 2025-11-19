65fec203c82f5941abb26195a19260ba51e73dce
victor <vhernandezt24@fpcoruna.afundacion.org>
2025-11-19 14:35:47 +0100

    segund endpoint

--- sprint3django/RestAPI/RestAPI/urls.py
+++ sprint3django/RestAPI/RestAPI/urls.py
@@ -22,5 +22,6 @@ from webserviceapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test', views.pagina_de_prueba),
-    path('juegos', views.devolver_juegos) 
+    path('juegos', views.devolver_juegos),
+    path('juegos/<int:id_solicitado>', views.devolver_juego_por_id) 
 ]
--- sprint3django/RestAPI/webserviceapp/views.py
+++ sprint3django/RestAPI/webserviceapp/views.py
@@ -19,4 +19,19 @@ def devolver_juegos(request):
 		respuesta_final.append(diccionario)
 	return JsonResponse(respuesta_final, safe=False)
 
-
+def devolver_juego_por_id(request, id_solicitado):
+	juego = Tjuegos.objects.get(id = id_solicitado)
+	comentarios = juego.tcomentarios_set.all()
+	lista_comentarios = []
+	for fila_comentario_sql in comentarios:
+		diccionario = {}
+		diccionario['id'] = fila_comentario_sql.id
+		diccionario['comentario'] = fila_comentario_sql.comentario
+		lista_comentarios.append(diccionario)
+	resultado = {
+		'id':juego.id,
+		'nombre': juego.nombre,
+		'url_imagen': juego.url_imagen,
+		'comentarios': lista_comentarios
+	}
+	return JsonResponse(resultado, json_dumps_params={'ensure_ascii': False}) 
