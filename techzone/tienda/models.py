201506b1a89c702e5f14fa40c967fe6608e6a3b6
victor <vhernandezt24@fpcoruna.afundacion.org>
2026-01-14 19:22:44 +0100

    feat: crear modelo Categoria inicial

--- techzone/tienda/models.py
+++ techzone/tienda/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
+
+    def __str__(self):
+        return self.nombre
