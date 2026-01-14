f4d557d2541820258991acb91448cf80b6d2ccbb
victor <vhernandezt24@fpcoruna.afundacion.org>
2026-01-14 19:41:08 +0100

    api: endpoint GET para listar categorias

--- techzone/tienda/views.py
+++ techzone/tienda/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Categoria
 
-# Create your views here.
+
+def lista_categorias(request):
+    # 1. Recuperamos los datos que acabas de crear en el admin
+    categorias = Categoria.objects.all()
+
+    # 2. Elegimos qué campos queremos enviar (id, nombre y slug)
+    data = list(categorias.values('id', 'nombre', 'slug'))
+
+    # 3. Respondemos con JSON
+    return JsonResponse(data, safe=False)
