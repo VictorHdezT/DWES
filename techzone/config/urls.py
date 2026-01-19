72549485e4c59332d16f56722adbad47b3d827aa
victor <vhernandezt24@fpcoruna.afundacion.org>
2026-01-19 09:55:55 +0100

    feat: endpoint productos usando APIView manual

--- techzone/config/urls.py
+++ techzone/config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from tienda.views import lista_categorias  # Importamos tu nueva vista
+from tienda.views import lista_categorias, ProductoListAPIView # Importamos la clase
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Esta es la dirección web que escribiremos en el navegador
     path('api/categorias/', lista_categorias),
+    # Nueva URL para la clase
+    path('api/productos/', ProductoListAPIView.as_view()),
 ]
-
--- techzone/tienda/views.py
+++ techzone/tienda/views.py
@@ -1,5 +1,8 @@
 from django.http import JsonResponse
 from .models import Categoria
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Producto
 
 
 def lista_categorias(request):
@@ -11,3 +14,22 @@ def lista_categorias(request):
 
     # 3. Respondemos con JSON
     return JsonResponse(data, safe=False)
+
+
+class ProductoListAPIView(APIView):
+    def get(self, request):
+        # 1. Buscar datos en la BD
+        productos = Producto.objects.all()
+
+        # 2. Convertir a lista de diccionarios
+        data = []
+        for p in productos:
+            data.append({
+                'id': p.id,
+                'nombre': p.nombre,
+                'precio': p.precio,
+                'activo': p.activo
+            })
+
+        # 3. Responder con Response (DRF se encarga del JSON)
+        return Response(data)
